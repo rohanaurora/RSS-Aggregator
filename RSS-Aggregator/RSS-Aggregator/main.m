3370dd47fdc40810fcfe526c5a7d4ec735ebac83
Rohan Aurora <rohanaurora@gmail.com>
2015-01-23 23:53:03 -0600

    Initial commit. Project set up.

---
+++ RSS-Aggregator/RSS-Aggregator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSS-Aggregator
+//
+//  Created by Rohan Aurora on 1/23/15.
+//  Copyright (c) 2015 Rohan Aurora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
