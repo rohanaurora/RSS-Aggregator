61bd60ba5dab68b2e723d78e89b33aafaac8edae
Rohan Aurora <rohanaurora@gmail.com>
2015-03-26 21:34:14 -0500

    Start new project.

---
+++ RSS-Catalog/RSS-Catalog/RSS-Catalog/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  RSS-Catalog
+//
+//  Created by Rohan Aurora on 4/3/15.
+//  Copyright (c) 2015 Rohan Aurora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ RSS-Catalog/RSS-Catalog/RSS-Catalog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSS-Catalog
+//
+//  Created by Rohan Aurora on 4/3/15.
+//  Copyright (c) 2015 Rohan Aurora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
