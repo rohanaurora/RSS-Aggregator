6601d8a932b1543948c4e945a6a7fd6cfeae7317
Rohan Aurora <rohanaurora@gmail.com>
2015-03-28 22:50:21 -0500

    Add code to read sqlite.db.

--- RSS-Catalog/RSS-Catalog/RSS-Catalog/RSSMasterViewController.m
+++ RSS-Catalog/RSS-Catalog/RSS-Catalog/RSSMasterViewController.m
@@ -9,88 +9,184 @@
 #import "RSSMasterViewController.h"
 #import "RSSDetailViewController.h"
 
-@interface RSSMasterViewController ()
+@interface RSSMasterViewController () {
+    RSSDB * _rssDB;
+}
 
-@property NSMutableArray *objects;
+@property (nonatomic, assign) BOOL isPad;
+@property (nonatomic, strong) NSArray *feedIDs;
+@property (nonatomic, strong) NSDictionary *newsFeed;
+
 @end
 
 @implementation RSSMasterViewController
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        self.clearsSelectionOnViewWillAppear = NO;
-        self.preferredContentSize = CGSizeMake(320.0, 600.0);
-    }
-}
+#pragma - View Lifecycle
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:0.5 green:0.0 blue:0.0 alpha:0.8];
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
-    self.detailViewController = (RSSDetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
+    self.title = @"RSS Catalog";
 }
 
-- (void)didReceiveMemoryWarning {
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    if (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad) self.isPad = YES;
+    [self loadFeedIDs];
+    [self.tableView reloadData];
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:YES];
+    if (self.newsFeed) [self loadNewFeed];
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    // allow upside-down orientation
+    return UIInterfaceOrientationMaskAll;
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender {
-    if (!self.objects) {
-        self.objects = [[NSMutableArray alloc] init];
-    }
-    [self.objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+#pragma mark - Segue delegate
+
+//- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+//    if ([segue.identifier isEqualToString:@"ToItemTableView"]) {
+//        BWRSSItemsTableViewController * itemsTableViewController = [segue destinationViewController];
+//        NSIndexPath * path = [self.tableView indexPathForSelectedRow];
+//        NSNumber * feedID = _feedIDs[path.row];
+//        
+//        // setup some context
+//        itemsTableViewController.currentFeedID = feedID;
+//        itemsTableViewController.rssDB = _rssDB;
+//    } else if([segue.identifier isEqualToString:@"ToAddView"]) {
+//        BWRSSAddViewController *rssAddViewController = [segue destinationViewController];
+//        rssAddViewController.delegate = self;
+//    }
+//}
+
+#pragma mark - RSSAddViewControllerDelegate delegate methods
+
+-(void) haveAddViewRecord:(NSDictionary *) avRecord {
+    self.newsFeed = avRecord;
+    if (self.isPad) [self loadNewFeed];
 }
 
-#pragma mark - Segues
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if ([[segue identifier] isEqualToString:@"showDetail"]) {
-        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = self.objects[indexPath.row];
-        RSSDetailViewController *controller = (RSSDetailViewController *)[[segue destinationViewController] topViewController];
-        [controller setDetailItem:object];
-        controller.navigationItem.leftBarButtonItem = self.splitViewController.displayModeButtonItem;
-        controller.navigationItem.leftItemsSupplementBackButton = YES;
-    }
+-(void) haveAddViewError:(NSError *) error {
+    NSString *errorMessage = [error localizedDescription];
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"URL Error" message:errorMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alertView show];
 }
 
-#pragma mark - Table View
+-(void) haveAddViewMessage:(NSString *)message {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"RSS Error" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alertView show];
+}
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.objects.count;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    [self loadFeedIDs];     // this gets called on reloadData so we must get a new count every time
+    return [_feedIDs count];
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-
-    NSDate *object = self.objects[indexPath.row];
-    cell.textLabel.text = [object description];
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"RSSCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+    
+    [self loadFeedIDsIfEmpty];
+    
+    // Configure the cell
+    NSDictionary * feedRow = [_rssDB getFeedRow:_feedIDs[indexPath.row]];
+    cell.textLabel.font = [UIFont boldSystemFontOfSize:[UIFont labelFontSize]];
+    cell.detailTextLabel.font = [UIFont systemFontOfSize:[UIFont smallSystemFontSize]];
+    [cell.textLabel setText: feedRow[@"title"]];
+    [cell.detailTextLabel setText: feedRow[@"desc"]];
+    
     return cell;
 }
 
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     return YES;
 }
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self loadFeedIDsIfEmpty];
+    
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [self.objects removeObjectAtIndex:indexPath.row];
+        // must update the database before updating the tableView
+        // so that the tableView never has a row that's missing from the database
+        [_rssDB deleteFeedRow:_feedIDs[indexPath.row]];
+        [self loadFeedIDs];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
     }
 }
+
+#pragma mark - Database methods
+
+- (void) loadNewFeed {
+    if (!self.newsFeed) return;
+    
+    NSDictionary * newsFeed = self.newsFeed;
+    newsFeed = nil;
+    
+    NSNumber * rc = [_rssDB addFeedRow:newsFeed];
+    NSIndexPath * idx = [self indexPathForDBRec:newsFeed];
+    if (rc == nil) {    // inserted row in DB
+        [self.tableView insertRowsAtIndexPaths:@[idx] withRowAnimation:UITableViewRowAnimationLeft];
+    }
+    [self.tableView scrollToRowAtIndexPath:idx atScrollPosition:UITableViewScrollPositionNone animated:YES];
+    if (rc != nil) {    // updated existing row in DB
+        [self.tableView reloadRowsAtIndexPaths:@[idx] withRowAnimation:UITableViewRowAnimationLeft];
+    }
+}
+
+- (NSIndexPath *) indexPathForDBRec:(NSDictionary *) dbRec {
+    NSNumber * rowID = [_rssDB valueFromQuery:@"SELECT id FROM feed WHERE url = ?", dbRec[@"url"]];
+    if (rowID) {
+        NSArray * tempFeedIDs = [_rssDB getFeedIDs];
+        return [NSIndexPath indexPathForRow:[tempFeedIDs indexOfObject:rowID] inSection:0];
+    } else {
+        return nil;
+    }
+}
+
+- (NSArray *) loadFeedIDs {
+    if (!_rssDB) [self loadFeedDB];
+    _feedIDs = [_rssDB getFeedIDs];
+    return _feedIDs;
+}
+
+- (NSArray *) loadFeedIDsIfEmpty {
+    if (!_rssDB) [self loadFeedDB];
+    if (!_feedIDs || ![_feedIDs count]) _feedIDs = [_rssDB getFeedIDs];
+    return _feedIDs;
+}
+
+- (RSSDB *) loadFeedDB {
+    if (!_rssDB) _rssDB = [[RSSDB alloc] initWithRSSDBFilename:@"bwrss.db"];
+    return _rssDB;
+}
+
+
 
 @end
